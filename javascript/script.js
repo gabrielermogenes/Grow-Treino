611fe5e29a242f367b715eae6e0d9aae2b0ff5e4
Gabriel Ermogenes <gabrielermogenes134@gmail.com>
2025-05-14 11:09:51 -0300

    Corrige conflitos e reorganiza arquivos

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 document.addEventListener("DOMContentLoaded", () => {
     const myObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
@@ -13,20 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const elements = document.querySelectorAll('section, div, .container, .planos, .precos, .billed, #billed-section main');
 
     elements.forEach((element) => myObserver.observe(element));
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    const myObserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            } else {
-                entry.target.classList.remove('show');
-            }
-        });
-    }, { threshold: 0.2 });
 
-    const elements = document.querySelectorAll('section, div, .container, .planos, .precos, .billed, #billed-section main');
-
-    elements.forEach((element) => myObserver.observe(element));
->>>>>>> 9f4a5d8f6bf7dc53cde9f3a252320c40c6444c4f
 });
